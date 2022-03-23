07ade9d0106f7b5dd4b205f737a3136c71eeaaf6
Alex Lanzano <lanzano.alex@gmail.com>
2022-03-22 21:10:19 -0400

    data: Add python script to convert captured points into framebuffer data. Add python script to visualize data

---
+++ data/data_converter.py
@@ -0,0 +1,87 @@
+#!/usr/bin/python
+
+import json
+import sys
+import copy
+
+def print_help():
+    print("data_converter.py [INPUT_FILE] [OUTPUT_FILE]")
+
+def swap(a, b):
+    return b, a
+
+def draw_point(framebuffer, point):
+    framebuffer[point[1]*240+point[0]] = 0
+    return framebuffer
+
+def draw_line(framebuffer, point1, point2):
+    if (point1 == point2):
+        return draw_point(framebuffer, point1)
+
+    x0 = point1[0]
+    y0 = point1[1]
+    x1 = point2[0]
+    y1 = point2[1]
+
+    steep = abs(y1 - y0) > abs(x1 - x0)
+
+    if (steep):
+        x0, y0 = swap(x0, y0)
+        x1, y1 = swap(x1, y1)
+
+    if (x0 > x1):
+        x0, x1 = swap(x0, x1)
+        y0, y1 = swap(y0, y1)
+
+    dx = x1 - x0
+    dy = abs(y1 - y0)
+
+    err = dx / 2
+    if y0 < y1:
+        ystep = 1;
+    else:
+        ystep = -1
+
+    while x0 <= x1:
+        if (steep):
+            framebuffer = draw_point(framebuffer, [y0, x0])
+        else:
+            framebuffer = draw_point(framebuffer, [x0, y0])
+        err -= dy
+        if (err < 0):
+            y0 += ystep
+            err += dx
+        x0 += 1
+
+    return framebuffer
+
+def main():
+    if len(sys.argv) < 3:
+        print_help()
+        return
+
+    input_file = sys.argv[1]
+    output_file = sys.argv[2]
+    output_dataset = {'data': []}
+    output_data = {'label': 'A',
+                   'framebuffer': []}
+
+    with open(input_file, 'r') as json_file:
+        input_dataset = json.load(json_file)
+        for input_data in input_dataset['data']:
+            output_data['framebuffer'] = [1] * (240*240)
+            for points in input_data['points']:
+                prev_point = None
+                for point in points:
+                    if (prev_point == None):
+                        output_data['framebuffer'] = draw_point(output_data['framebuffer'], point)
+                    else:
+                        output_data['framebuffer'] = draw_line(output_data['framebuffer'], prev_point, point);
+                    prev_point = point
+            new_data = copy.deepcopy(output_data)
+            output_dataset['data'].append(new_data)
+
+    with open(output_file, 'w') as json_file:
+        json.dump(output_dataset, json_file)
+
+main()
---
+++ data/data_viewer.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python
+
+import sys
+import json
+import pygame
+from pygame.locals import *
+
+def print_help():
+    print("data_viewer.py [INPUT_FILE] [DATA_INDEX]")
+
+def pixel_to_color(pixel):
+    if (pixel == 1):
+        return (255, 255, 255)
+    else:
+        return (255, 0, 0)
+
+
+def render_framebuffer(surface, framebuffer):
+    x = 0
+    y = 0
+    rect = [0, 0, 3, 3]
+    for pixel in framebuffer:
+        color = pixel_to_color(pixel)
+        rect[0] = x * 3
+        rect[1] = y * 3
+        pygame.draw.rect(surface, color, rect)
+        if (x < 239):
+            x += 1
+        else:
+            x = 0
+            y += 1
+    pygame.display.flip()
+
+def main():
+    if len(sys.argv) < 3:
+        print_help()
+        return
+
+    pygame.init()
+    surface = pygame.display.set_mode((720, 720), 0, 32)
+    input_file = sys.argv[1]
+    data_index = int(sys.argv[2])
+
+    with open(input_file, 'r') as json_file:
+        dataset = json.load(json_file)
+        if (len(dataset['data']) <= data_index):
+            print("Invalid index")
+            return
+        data = dataset['data'][data_index]
+        render_framebuffer(surface, data['framebuffer'])
+
+    while True:
+        a = 1
+
+main()
